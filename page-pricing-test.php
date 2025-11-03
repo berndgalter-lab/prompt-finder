3a09f02da1aac15ec6f9e5c33c3d301026fb20b7
Bernd Galter <berndgalter-lab@users.noreply.github.com>
2025-11-03 19:05:17 +0100

    back to roots

---
+++ page-pricing-test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Pricing Test
+ */
+
+if (!defined('ABSPATH')) { exit; }
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+  <div class="pf-wrap">
+    <h1>Pricing Test - Working!</h1>
+    <p>If you see this, the basic template works.</p>
+  </div>
+</main>
+
+<?php get_footer(); ?>
+
